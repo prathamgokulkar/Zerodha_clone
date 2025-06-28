2b1c76e0349bf4bcad4ad548dea451a212c325d8
Prathamesh Gokulkar <prathamgokulkar@gmail.com>
2025-06-28 15:48:06 +0530

    backend setup

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+
+app.listen(8080, () => {
+  console.log("Server is running on port 3000");
+});
